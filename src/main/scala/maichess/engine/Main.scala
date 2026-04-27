7937e5446eafaeba84ae30d668d102b5f02ef760
Tom Berger <berger.tom@mail.de>
2026-04-27 21:08:58 +0200

    feat: telemetry

--- src/main/scala/maichess/engine/Main.scala
+++ src/main/scala/maichess/engine/Main.scala
@@ -1,7 +1,17 @@
 package maichess.engine
 
-import io.grpc.{Server, ServerBuilder}
+import io.grpc.{Server, ServerBuilder, ServerInterceptor}
 import io.grpc.stub.StreamObserver
+import io.opentelemetry.api.common.Attributes
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator
+import io.opentelemetry.context.propagation.ContextPropagators
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter
+import io.opentelemetry.instrumentation.grpc.v1_6.GrpcTelemetry
+import io.opentelemetry.sdk.OpenTelemetrySdk
+import io.opentelemetry.sdk.resources.Resource
+import io.opentelemetry.sdk.trace.SdkTracerProvider
+import io.opentelemetry.sdk.trace.`export`.BatchSpanProcessor
+import io.opentelemetry.semconv.ServiceAttributes
 import scala.concurrent.Future
 import zio.{Runtime, Unsafe, ZIO, ZIOAppDefault}
 import maichess.engine.grpc.BotsServiceImpl
@@ -16,23 +26,52 @@ import maichess.engine.v1.bots.bots.{
   ListBotsResponse,
 }
 
+@SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
 object Main extends ZIOAppDefault:
 
   private val port: Int =
     sys.env.get("GRPC_PORT").flatMap(_.toIntOption).getOrElse(50056)
 
-  def run =
-    ZIO.serviceWithZIO[BotsServiceImpl] { svc =>
-      ZIO.runtime[Any].flatMap { runtime =>
-        ZIO.acquireReleaseWith(
-          ZIO.attempt(startServer(svc, runtime))
-        )(s => ZIO.attempt(s.shutdown()).orDie) { _ =>
-          ZIO.logInfo(s"gRPC server listening on port $port") *> ZIO.never
-        }
-      }
-    }.provide(BotsServiceImpl.layer, EngineServiceLive.layer)
+  private val otlpEndpoint: String =
+    sys.env.getOrElse("OTEL_EXPORTER_OTLP_ENDPOINT", "http://otel-collector:4317")
 
-  private def startServer(svc: BotsServiceImpl, runtime: Runtime[Any]): Server =
+  private def buildTracerProvider(): SdkTracerProvider =
+    val resource = Resource.getDefault().merge(
+      Resource.create(Attributes.of(ServiceAttributes.SERVICE_NAME, "engine-service")))
+    val exporter = OtlpGrpcSpanExporter.builder().setEndpoint(otlpEndpoint).build()
+    SdkTracerProvider.builder()
+      .setResource(resource)
+      .addSpanProcessor(BatchSpanProcessor.builder(exporter).build())
+      .build()
+
+  private def buildGrpcTelemetry(tracerProvider: SdkTracerProvider): GrpcTelemetry =
+    val otel = OpenTelemetrySdk.builder()
+      .setTracerProvider(tracerProvider)
+      .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+      .buildAndRegisterGlobal()
+    GrpcTelemetry.create(otel)
+
+  def run =
+    ZIO.acquireReleaseWith(
+      ZIO.attempt(buildTracerProvider())
+    )(tp => ZIO.attempt(tp.close()).orDie) { tracerProvider =>
+      val grpcTelemetry = buildGrpcTelemetry(tracerProvider)
+      ZIO.serviceWithZIO[BotsServiceImpl] { svc =>
+        ZIO.runtime[Any].flatMap { runtime =>
+          ZIO.acquireReleaseWith(
+            ZIO.attempt(startServer(svc, runtime, grpcTelemetry.newServerInterceptor()))
+          )(s => ZIO.attempt(s.shutdown()).orDie) { _ =>
+            ZIO.logInfo(s"gRPC server listening on port $port") *> ZIO.never
+          }
+        }
+      }.provide(BotsServiceImpl.layer, EngineServiceLive.layer)
+    }
+
+  private def startServer(
+    svc: BotsServiceImpl,
+    runtime: Runtime[Any],
+    interceptor: ServerInterceptor,
+  ): Server =
     ServerBuilder
       .forPort(port)
       .addService(
@@ -41,6 +80,7 @@ object Main extends ZIOAppDefault:
           scala.concurrent.ExecutionContext.global,
         )
       )
+      .intercept(interceptor)
       .build()
       .start()
 
